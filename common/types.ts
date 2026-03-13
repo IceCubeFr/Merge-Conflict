79160f9539a9f0d62826dfe726e5712f9c0afa70
Sebastian Novak <sebastian.novak.etu@univ-lille.fr>
2026-03-13 09:48:20 +0100

    Added Player Class

--- common/types.ts
+++ common/types.ts
@@ -12,3 +12,29 @@ export interface BestScore {
     score:number;
     date:Date;
 }
+
+export default class Player {
+    health: number;
+    score: number;
+    models = [];
+
+    constructor() {
+        this.health = 3;
+        this.score = 0;
+    }
+
+    takeHealth() {
+        this.health--;
+    }
+
+    verifyHealth() {
+        if(this.health == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    giveHealth(hp:number) {
+        this.health += hp;
+    }
+}
